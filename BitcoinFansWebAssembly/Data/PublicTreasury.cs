56ffab1f161bdf8b10526a98857642a8310cbda6
AdamKiniouar <adam.kiniouar@alimakgroup.com>
2021-08-27 16:19:35 +0200

    Added PublicTreasury class

---
+++ BitcoinFansWebAssembly/Data/PublicTreasury.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitcoinFansWebAssembly.Data
+{
+    public class PublicTreasury
+    {
+        public float total_holdings { get; set; }
+        public float total_value_usd { get; set; }
+        public float market_cap_dominance { get; set; }
+        public Company[] companies { get; set; }
+    }
+
+    public class Company
+    {
+        public string name { get; set; }
+        public string symbol { get; set; }
+        public string country { get; set; }
+        public int total_holdings { get; set; }
+        public long total_entry_value_usd { get; set; }
+        public long total_current_value_usd { get; set; }
+        public float percentage_of_total_supply { get; set; }
+    }
+
+}
