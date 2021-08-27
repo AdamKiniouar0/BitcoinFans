0db5f7edff1fe5a8aafc5b25a9ae63dff01bfe7c
AdamKiniouar <adam.kiniouar@alimakgroup.com>
2021-08-27 10:23:25 +0200

    Added getjson method Added PriceObject class

---
+++ BitcoinFansWebAssembly/Data/PriceObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitcoinFansWebAssembly.Data
+{
+    public class PriceObject
+    {
+        public Bitcoin bitcoin { get; set; }
+        public Vechain vechain { get; set; }
+        public Ethereum ethereum { get; set; }
+    }
+
+    public class Bitcoin
+    {
+        public int sek { get; set; }
+        public int usd { get; set; }
+    }
+
+    public class Vechain
+    {
+        public float sek { get; set; }
+        public float usd { get; set; }
+    }
+
+    public class Ethereum
+    {
+        public int sek { get; set; }
+        public float usd { get; set; }
+    }
+}
+
